f22aa4113d9ec5ce6072440e927680ea571f25f9
LaroussiEssalha <151468645+LaroussiEssalha@users.noreply.github.com>
2024-02-27 20:07:15 +0100

    Exercice Rappel Java

---
+++ SoldeInsuffisantException.java
@@ -0,0 +1,11 @@
+//SoldeInsuffisantException.java
+
+public class SoldeInsuffisantException extends Exception{
+
+    @Override
+    public String toString() {
+        return "Solde Insuffisant!!!";
+    }
+    
+
+}
